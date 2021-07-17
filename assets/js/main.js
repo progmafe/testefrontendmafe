fa213d1e94a58d7105293f472ce3941ca7527e56
progmafe <fernandacruz_bueno@yahoo.com.br>
2021-07-17 18:55:53 -0300

    feat: setup carrossel e alinhamento ajustes header

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,23 @@
 
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+        loop:true,
+        nav: true,
+        dots:false,
+        navText: ["<",">"],
+        navElement: 'div',
+        navContainer: false,
+        navContainerClass:"depoimento-nav",
+        navClass: ["owl-prev","owl-next"],
+        responsive:{
+            0:{
+                items:1,
+                nav:false
+            },
+            600:{
+                items:1,
+                nav:true,
+            }
+        }
+    });
+});
